9b03d49a3fde9e50e0e59a92246f95dc3701a3c9
Ledun <hanluee@yandex.com>
2021-04-20 04:26:48 +0300

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { SectionsComponent } from './sections/sections.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+const routes: Routes =[
+    { path: 'home',component: HomeComponent},
+ 
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+      useHash: true
+    })
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
